7196faace23abd039b02c0dc7ae80ec5f2115dce
Mudathir M Muhammad <khalifasitemsn@gmail.com>
2024-04-23 12:07:27 +0100

    dbconnection

--- Assessment-system/dbConnection.php
+++ Assessment-system/dbConnection.php
@@ -1,21 +1,3 @@
-<!-- <?php
-// $con= new mysqli('localhost','root','','assessment_db')or die("Could not connect to mysql".mysqli_error($con));
-
-// MySQL connection parameters
-$hostname = 'localhost'; // Change this if your MySQL server is running on a different host
-$username = 'root';      // MySQL username
-$password = ''; // MySQL password, replace 'your_password' with the actual password
-$database = 'assessment_db';   // MySQL database name
-
-// Create a new MySQLi connection
-$con = new mysqli($hostname, $username, $password, $database);
-
-// Check for connection errors
-if ($con->connect_errno) {
-    // If connection fails, output the error message
-    die("Could not connect to MySQL: " . $con->connect_error);
-}
+<?php
+$con = new mysqli('localhost','root','','assessment_db')or die("Could not connect to mysql".mysqli_error($con));
 ?>
-
-
-?> -->
