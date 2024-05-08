eb4e3667e89d9726cc71aae50920e002dce80ad3
Mudathir M Muhammad <khalifasitemsn@gmail.com>
2024-05-08 13:15:24 +0100

    site done

--- Assessment-system/index.php
+++ Assessment-system/index.php
@@ -29,9 +29,13 @@ var b = document.forms["form"]["cpassword"].value;if (a!=b){alert("Passwords mus
 <div class="header">
 <div class="row">
 <div class="col-lg-6">
-<span class="logo">Learning Assessment</span></div>
+<span class="logo">Learning Assessment</span>
+</div>
 <div class="col-md-2 col-md-offset-4">
-<a href="#" class="pull-right btn sub1" data-toggle="modal" data-target="#myModal"><span class="glyphicon glyphicon-log-in" aria-hidden="true"></span>&nbsp;<span class="title1"><b>Login</b></span></a></div>
+<a href="#" class="pull-right btn sub1" data-toggle="modal" data-target="#myModal"><span class="glyphicon glyphicon-log-in" aria-hidden="true"></span>&nbsp;<span class="title1"><b>Login</b></span></a>
+</div>
+
+
 <!--sign in modal start-->
 <div class="modal fade" id="myModal"> 
   <div class="modal-dialog">
